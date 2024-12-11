1a71462060f4fd1f1f08578865d24135e039f3f9
Pascal-Benink <112872169+Pascal-Benink@users.noreply.github.com>
2024-12-11 14:38:58 +0100

    Remove commented-out subscription check in useOnPlay hook

--- hooks/useOnPlay.ts
+++ hooks/useOnPlay.ts
@@ -15,10 +15,10 @@ const useOnPlay = (songs: Song[]) => {
         {
             return authModal.onOpen();
         }
-        if (!subscription)
-        {
-            return subscribeModal.onOpen();
-        }
+        // if (!subscription)
+        // {
+        //     return subscribeModal.onOpen();
+        // }
 
         player.setId(id);
         player.setIds(songs.map((song) => song.id));
