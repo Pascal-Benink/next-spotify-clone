ed5222366c5745fe170a4c95eb917a15efc26c80
Pascal-Benink <9018437@student.zadkine.nl>
2024-12-16 09:16:57 +0100

    Add access control to getPlaylist action to restrict access to private playlists

--- actions/getPlaylist.ts
+++ actions/getPlaylist.ts
@@ -7,15 +7,25 @@ const getPlaylist = async (id: string): Promise<Playlist> => {
         cookies: cookies
     });
 
+    const {
+        data: {
+            session
+        }
+    } = await supabase.auth.getSession();
+
     const { data, error } = await supabase
-    .from('playlists')
-    .select('*')
-    .eq('id', id)
-    .order('created_at', { ascending: false })
-    .single();
+        .from('playlists')
+        .select('*')
+        .eq('id', id)
+        .order('created_at', { ascending: false })
+        .single();
 
     if (error) {
         console.error(error);
+    }
+
+    if (!data || (data.user_id !== session?.user.id && !data.is_public)) {
+        throw new Error("You do not have access to this playlist.");
     }
 
     return (data as any) || [];
