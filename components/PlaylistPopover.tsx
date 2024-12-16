920d1bf982cecd99d2cfa9fb1f27edb815ca8bdb
Pascal-Benink <9018437@student.zadkine.nl>
2024-12-16 15:30:42 +0100

    Add delete playlist option to PlaylistPopover component

--- components/PlaylistPopover.tsx
+++ components/PlaylistPopover.tsx
@@ -2,7 +2,8 @@
 
 import React from "react";
 import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
-import { FaEllipsisH } from "react-icons/fa";;
+import { FaEllipsisH } from "react-icons/fa";import { HiOutlineTrash } from "react-icons/hi";
+;
 
 const PlaylistPopover = () => {
 
@@ -10,16 +11,18 @@ const PlaylistPopover = () => {
         <div>
             <DropdownMenu.Root>
                 <DropdownMenu.Trigger asChild>
-                    <button
-                    >
+                    <button>
                         <FaEllipsisH color={'white'} size={25} />
                     </button>
                 </DropdownMenu.Trigger>
 
                 <DropdownMenu.Portal>
-                    <DropdownMenu.Content>
-                        <DropdownMenu.Item>
-                            Delete Playlist
+                    <DropdownMenu.Content
+                        className="py-2 min-w-[220px] rounded-md bg-neutral-900 p-[5px] shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),_0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)] will-change-[opacity,transform] data-[side=bottom]:animate-slideUpAndFade data-[side=left]:animate-slideRightAndFade data-[side=right]:animate-slideLeftAndFade data-[side=top]:animate-slideDownAndFade"
+                    >
+                        <DropdownMenu.Item className="flex flex-row justify-between cursor-pointer focus:outline-none hover:text-white px-3" >
+                            Delete Playlist <HiOutlineTrash  size={20} className="text-neutral-400" /> 
+                            {/* TODO: make a delete playlist action */}
                         </DropdownMenu.Item>
                     </DropdownMenu.Content>
                 </DropdownMenu.Portal>
