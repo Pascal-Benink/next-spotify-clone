5c8481ea725e6da232cd0ef688c2e62fd48fe9c1
Pascal-Benink <112872169+Pascal-Benink@users.noreply.github.com>
2024-12-20 12:13:39 +0100

    Add PlaylistButton to LikedContent for enhanced song management

--- app/liked/components/LikedContent.tsx
+++ app/liked/components/LikedContent.tsx
@@ -8,6 +8,7 @@ import { useEffect } from "react";
 import MediaItem from "@/components/MediaItem";
 import LikeButton from "@/components/LikeButton";
 import useOnPlay from "@/hooks/useOnPlay";
+import PlaylistButton from "@/components/PlaylistButton";
 
 interface LikedContentProps {
     songs: Song[];
@@ -44,25 +45,26 @@ const LikedContent: React.FC<LikedContentProps> = ({
         )
     }
 
-    return ( 
+    return (
         <div className="flex flex-col gap-y-2 w-full p-6">
             {songs.map((song) => (
                 <div
-                key={song.id}
-                className="flex items-center gap-x-4 wq-full"
+                    key={song.id}
+                    className="flex items-center gap-x-4 wq-full"
                 >
                     <div className="flex-1">
-                        <MediaItem 
-                        onClick={(id: string) => {onPlay(id)}}
-                        data={song}
-                        isOwner={song.user_id === userId}
+                        <MediaItem
+                            onClick={(id: string) => { onPlay(id) }}
+                            data={song}
+                            isOwner={song.user_id === userId}
                         />
                     </div>
+                    <PlaylistButton songId={song.id} />
                     <LikeButton songId={song.id} />
                 </div>
             ))}
         </div>
-     );
+    );
 }
- 
+
 export default LikedContent;
