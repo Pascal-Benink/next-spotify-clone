a30b828c02271dff3aaebb5e342894afaa48efaa
Pascal-Benink <112872169+Pascal-Benink@users.noreply.github.com>
2024-12-20 11:03:17 +0100

    Enhance LyricsPage with a header and improved no lyrics message

--- app/(site)/lyrics/[songId]/page.tsx
+++ app/(site)/lyrics/[songId]/page.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from 'react';
 import { useSupabaseClient } from '@supabase/auth-helpers-react';
 import getSongsLyricsById from '@/actions/getSongLyricsById';
+import Header from '@/components/Header';
 
 interface Props {
     params: {
@@ -39,7 +40,10 @@ const LyricsPage: React.FC<Props> = ({ params }) => {
 
     return (
         <div className="flex flex-col w-full bg-green-500 p-6 rounded-lg gap-3">
-            {Array.isArray(lyrics) ? (
+             <Header className='bg-gradient-to-b from-green-500 p-6 no-drop-shadow'>  
+                <h2 className='font-semibold text-2xl'>Lyrics</h2>
+            </Header>
+            {Array.isArray(lyrics) && lyrics.length > 0 ? (
                 lyrics.map((data) => (
                     data.lyrics.split('\n').map((line, index) => (
                         <p className="font-bold text-[2.8rem]" key={`${data.id}-${index}`}>
@@ -48,7 +52,9 @@ const LyricsPage: React.FC<Props> = ({ params }) => {
                     ))
                 ))
             ) : (
-                <p>No lyrics found.</p>
+                <div className="flex justify-center items-center h-[100vh]">
+                    <p className="text-center font-semibold text-[1.5rem]">This song has no lyrics.</p>
+                </div>
             )}
         </div>
     );
