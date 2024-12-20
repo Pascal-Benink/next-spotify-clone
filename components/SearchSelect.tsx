1cc1f8cfa1a21b35b4a3c8a068722940d3cb88cd
Pascal-Benink <112872169+Pascal-Benink@users.noreply.github.com>
2024-12-20 14:39:33 +0100

    Refactor DebugPage to utilize SearchSelect component and remove unused data fetching

--- components/SearchSelect.tsx
+++ components/SearchSelect.tsx
@@ -1,5 +1,9 @@
+"use client";
+
 import * as Select from "@radix-ui/react-select";
+import React from "react";
 import { useState, forwardRef } from "react";
+import { FaCheck } from "react-icons/fa";
 import { twMerge } from "tailwind-merge";
 
 export type SelectType = {
@@ -27,12 +31,11 @@ const SearchSelect = forwardRef<HTMLDivElement, SearchSelectProps>(
             <Select.Root onValueChange={onSelect} value={selected ?? undefined}>
                 <Select.Trigger className={twMerge("flex w-full rounded-md bg-neutral-700 border border-transparent px-3 py-3 text-sm placeholder:text-neutral-400 focus:outline-none", className)}>
                     <Select.Value placeholder={placeholder} />
-                    <Select.Icon />
+                    <Select.Icon className="absolute right-8" />
                 </Select.Trigger>
 
                 <Select.Portal>
-                    <Select.Content>
-                        <Select.ScrollUpButton />
+                    <Select.Content className="overflow-hidden rounded-md bg-neutral-800 shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)]">
                         <Select.Viewport>
                             <div style={{ padding: "8px" }}>
                                 <input
@@ -43,15 +46,22 @@ const SearchSelect = forwardRef<HTMLDivElement, SearchSelectProps>(
                                     className={twMerge("flex w-full rounded-md bg-neutral-700 border border-transparent px-3 py-3 text-sm placeholder:text-neutral-400 focus:outline-none")}
                                 />
                             </div>
-                            {filteredData.map((item, index) => (
-                                <Select.Item key={index} value={item.id}>
-                                    <Select.ItemText>{item.name}</Select.ItemText>
-                                    <Select.ItemIndicator />
-                                </Select.Item>
-                            ))}
+                            <Select.Group>
+                                <SelectItem value="leek" className="hidden">
+                                    leek
+                                </SelectItem>
+                                {filteredData.length === 0 && (
+                                    <SelectItem disabled value="none">
+                                        <Select.ItemText>No results found</Select.ItemText>
+                                    </SelectItem>
+                                )}
+                                {filteredData.map((item, index) => (
+                                    <SelectItem key={index} value={item.id}>
+                                        {item.name}
+                                    </SelectItem>
+                                ))}
+                            </Select.Group>
                         </Select.Viewport>
-                        <Select.ScrollDownButton />
-                        <Select.Arrow />
                     </Select.Content>
                 </Select.Portal>
             </Select.Root>
@@ -60,5 +70,32 @@ const SearchSelect = forwardRef<HTMLDivElement, SearchSelectProps>(
 );
 
 SearchSelect.displayName = 'SearchSelect';
+
+interface SelectItemProps {
+    children: React.ReactNode;
+    className?: string;
+    value: string;
+    disabled?: boolean;
+}
+
+const SelectItem = React.forwardRef<HTMLDivElement, SelectItemProps>(
+    ({ children, className, ...props }, forwardedRef) => {
+        return (
+            <Select.Item
+                className={twMerge(
+                    "relative flex h-[25px] select-none items-center rounded-[3px] pl-[25px] pr-[35px] text-[13px] leading-none text-green-500 data-[disabled]:pointer-events-none data-[highlighted]:bg-green-500 data-[disabled]:text-mauve8 data-[highlighted]:text-violet1 data-[highlighted]:outline-none",
+                    className,
+                )}
+                {...props}
+                ref={forwardedRef}
+            >
+                <Select.ItemText>{children}</Select.ItemText>
+                <Select.ItemIndicator className="absolute left-0 inline-flex w-[25px] items-center justify-center">
+                    <FaCheck />
+                </Select.ItemIndicator>
+            </Select.Item>
+        );
+    },
+);
 
 export default SearchSelect;
