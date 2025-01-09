6723140fb262839a2ce89712cfa2b44d10ebe0ce
Pascal-Benink <112872169+Pascal-Benink@users.noreply.github.com>
2025-01-09 11:11:09 +0100

    Add empty state message for PlaylistContent when no playlists are available

--- app/(site)/components/PlaylistContent.tsx
+++ app/(site)/components/PlaylistContent.tsx
@@ -7,6 +7,13 @@ interface PlaylistContentProps {
 }
 
 const PlaylistContent: React.FC<PlaylistContentProps> = ({ playlists, userId }) => {
+    if (playlists.length === 0) {
+        return (
+            <div className="mt-4 text-neutral-400">
+                No Playlists Available
+            </div>
+        )
+    }
     return (
         <div className="grid grid-rows-1 grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 lx:grid-cols-5 2xl:grid-cols-8 gap-4 ml-4">
             {playlists.slice(0, 16).map((item) => (
