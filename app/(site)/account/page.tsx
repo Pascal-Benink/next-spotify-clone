8f02a75173355d47182753d2cc843a95616479b4
Pascal-Benink <112872169+Pascal-Benink@users.noreply.github.com>
2025-01-10 10:01:19 +0100

    Refactor account components: replace AccountContent with BillingContent and InfoContent

--- app/(site)/account/page.tsx
+++ app/(site)/account/page.tsx
@@ -1,28 +1,21 @@
 import Header from "@/components/Header";
-import AccountContent from "./components/AccountContent";
+import { BillingContent } from "./components/BillingContent";
+import { InfoContent } from "./components/InfoContent";
 
-const account = () => {
-    return (
-        <div
-            className="
-        bg-neutral-900
-        rounded-lg
-        w-full
-        h-full
-        overflow-hidden
-        overflow-y-auto
-        "
-        >
+const AccountPage = () => {
+    return ( 
+        <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
             <Header className="from-bg-neutral-900">
                 <div className="mb-2 flex flex-col gap-y-6">
-                    <h1 className="text-white text-3xl font-bold">
+                    <h1 className="text-white text-3xl font-semibold">
                         Account Settings
                     </h1>
                 </div>
             </Header>
-            <AccountContent />
+            <BillingContent/>
+            <InfoContent/>
         </div>
-    );
+     );
 }
-
-export default account;
+ 
+export default AccountPage;
